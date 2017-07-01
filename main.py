1860dc012a761d5a4b75edd00c8d3147d7ca260a
Denis Kalinochkin <kalinochkind@gmail.com>
2017-07-01 14:33:19 +0300

    Все починено

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import sys
 import re
 
 ROLL_INTERVAL = 5
+MAX_LEVEL = 3
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2763.0 Safari/537.36'
 
 
@@ -22,15 +23,17 @@ class Bot:
         self.last_roll = 0
         resp = ''
         try:
-            self.logins = []
             self.ids = []
+            self.player_cache = {}
             for i in range(len(self.openers)):
                 resp = self.open('getthis', {}, opener=i)
-                self.logins.append(json.loads(resp)['name'])
-                self.ids.append(json.loads(resp)['id'])
+                self.ids.append(str(json.loads(resp)))
             self.fetchCountryNames()
+            self.fractions = json.loads(self.open('factions'))
+            self.fractions['0'] = {'name': '<none>'}
             self.getMapInfo()
-            self.last_error = dict.fromkeys(self.logins)
+            self.getPlayers(self.ids)
+            self.last_error = {}
         except Exception as e:
             print(resp or 'The server is down!')
             sys.exit(1)
@@ -40,14 +43,20 @@ class Bot:
         d = json.loads(s)[2]['paths']
         self.country_name = {i: d[i]['name'] for i in d}
 
+    def getPlayers(self, players):
+        players = [i for i in map(str, players) if i not in self.player_cache]
+        if players:
+            resp = self.open('getplayers?ids=[{}]'.format('%2C'.join(players)))
+            pg = json.loads(resp)
+            for i in pg:
+                pg[i]['fid'] = str(pg[i].get('fid', 0))
+                pg[i]['id'] = str(pg[i]['id'])
+            self.player_cache.update(pg)
+
     def sorted_map(self):
-        func = {'m': lambda x: -self.map[x][3] / self.map[x][1],
-                'M': lambda x: self.map[x][3] / self.map[x][1],
-                'l': lambda x: self.map[x][1],
-                'L': lambda x: -self.map[x][1],
-                's': lambda x: -self.map[x][3],
-                'S': lambda x: self.map[x][3],
-                'e': lambda x: -self.map[x][3],}
+        func = {'m': lambda x: self.map[x][1],
+                'M': lambda x: -self.map[x][1],
+                'e': lambda x: self.map[x][1],}
         return sorted(self.map, key=lambda x:(func[self.order](x), x))
 
     def auth(self):
@@ -56,20 +65,6 @@ class Bot:
             self.openers.append(requests.session())
             self.openers[-1].mount(self.host, hyper.contrib.HTTP20Adapter())
             self.openers[-1].cookies['session'] = session
-
-
-    def genCode(self, country):
-        r = random.random()
-        a = 'cyka' + country + str(r)
-        b = 0
-        for i in a:
-            b *= 31
-            b += ord(i)
-            while b < -2**31:
-                b += 2**32
-            while b > 2 ** 31 - 1:
-                b -= 2**32
-        return {'a': b, 'b': r}
 
     def open(self, uri, params=None, opener=0):
         headers = {'Connection': None, 'User-agent': USER_AGENT}
@@ -89,29 +84,23 @@ class Bot:
         return ''
 
     def getMapInfo(self):
-        pg = json.loads(self.open('gethint', {}))
+        pg = json.loads(self.open('get', {}))
         self.map = {}
-        for i in pg:
-            self.map[i] = (pg[i]['User'], pg[i]['sp'], pg[i]['Faction'] or '<none>', float(pg[i]['relsize']))
+        for i in pg['map']:
+            self.map[i] = (str(pg['map'][i]['uid']), pg['map'][i]['sp'])
+        users = pg['players']
+        for i in users:
+            users[i]['id'] = str(users[i]['id'])
+            users[i]['fid'] = str(users[i].get('fid', 0))
+        self.player_cache.update(users)
 
     def getPlayerList(self):
-        res = self.open('getplayers')
-        pg = json.loads(res)
         all_users = {i[0] for i in self.map.values()}
-        res = []
-        for i in pg:
-            for j in all_users:
-                if i['name'].startswith(j):
-                    i['name'] = j
-                    res.append(i)
-                    break
-        return res
+        return [self.player_cache[i] for i in all_users]
 
     def fight(self, country):
         try:
-            d = self.genCode(country)
-            d['target'] = country
-            d['captcha'] = ''
+            d = {'target': country, 'captcha': ''}
             ctime = time.time()
             if ctime < self.last_roll + ROLL_INTERVAL:
                 time.sleep(self.last_roll + ROLL_INTERVAL - ctime)
@@ -139,15 +128,15 @@ class Bot:
                         return
                     else:
                         self.getMapInfo()
-                        if self.map[country][0] in self.logins:
-                            if self.map[country][1] == 7:
+                        if self.map[country][0] in self.ids:
+                            if self.map[country][1] >= MAX_LEVEL:
                                 print('Finished')
                                 return
                 elif res['result'] == 'fail':
                     print('.', end='', flush=True)
                 elif res['result'] == 'error':
                     if res['data'] != self.last_error[i] and res['data'] != 'Подождите немного!':
-                        print('[{} - {}]'.format(self.logins[i], res['data']), end='', flush=True)
+                        print('[{} - {}]'.format(self.player_cache[self.ids[i]]['name'], res['data']), end='', flush=True)
                     self.last_error[i] = res['data']
                 self.getMapInfo()
         except Exception as e:
@@ -156,25 +145,25 @@ class Bot:
             return False
 
     def conquerCountry(self, country):
-        if self.map[country][0].lower() in self.logins:
+        if self.map[country][0] in self.ids:
             return
-        print('\nConquering {} ({}), level {}, belongs to {}'.format(country, self.country_name[country], self.map[country][1], self.map[country][0]))
-        while self.map[country][0].lower() not in self.logins:
+        print('\nConquering {} ({}), level {}, belongs to {}'.format(country, self.country_name[country], self.map[country][1], self.player_cache[self.map[country][0]]['name']))
+        while self.map[country][0].lower() not in self.ids:
             self.fight(country)
         self.sendToBatya(country)
 
     def empowerCountry(self, country):
-        if self.map[country][1] == 7 or self.map[country][0].lower() not in self.logins:
+        if self.map[country][1] >= MAX_LEVEL or self.map[country][0] not in self.ids:
             return
         print('\nEmpowering {} ({}), level {}'.format(country, self.country_name[country], self.map[country][1]))
-        while self.map[country][1] != 7:
+        while self.map[country][1] < MAX_LEVEL:
             self.fight(country)
 
     def conquer(self, object_list):
         self.getMapInfo()
         tmap = self.sorted_map()
         for name in tmap:
-            if name.lower() in object_list or self.map[name][0].lower() in object_list or '*' in object_list:
+            if self.map[name][0] in object_list or '*' in object_list:
                 if self.order == 'e':
                     self.empowerCountry(name)
                 else:
@@ -183,9 +172,9 @@ class Bot:
 
     def sendToBatya(self, country):
         pname = self.map[country][0]
-        if pname not in self.logins:
+        if pname not in self.ids:
             return
-        pid = self.logins.index(pname)
+        pid = self.ids.index(pname)
         if pid == 0:
             return
         self.open('give', json.dumps({'target': country, 'targetplid': self.ids[0]}), opener=pid)
@@ -207,8 +196,8 @@ def main():
         for i in bot.last_error:
             bot.last_error[i] = None
         users = bot.getPlayerList()
-        users = [(i['name'], sum(bot.map[j][0] == i['name'] for j in bot.map), i['clan'] or '<none>', i['id'],
-                  sum(bot.map[j][1] * (bot.map[j][0] == i['name']) for j in bot.map)) for i in users]
+        users = [(i['name'], sum(bot.map[j][0] == i['id'] for j in bot.map), bot.fractions[str(i.get('fid', 0))]['name'], i['id'],
+                  sum(bot.map[j][1] * (bot.map[j][0] == i['id']) for j in bot.map)) for i in users]
         print('Users on the map:\n' + '\n'.join('[{3:3}] {0}:{2} ({1}, {4})'.format(*i) for i in sorted(users, key=lambda x:(-x[1], -x[4]))))
         print()
         try:
@@ -216,14 +205,12 @@ def main():
         except EOFError:
             print()
             return
-        id_to_name = {i[3]: i[0].lower() for i in users}
         if c and len(c[0]) == 1 and c[0] in 'lLsSmMe':
             bot.order = c[0]
             c = c[1:]
         if not c:
             c = ['*']
         try:
-            c = [id_to_name.get(int(i)) if i.isdigit() else i.lower() for i in c]
             for country in bot.map:
                 bot.sendToBatya(country)
             bot.conquer(c)
